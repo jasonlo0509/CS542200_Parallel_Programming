4a3e0c912857dcc585198389298883ed6c2e875c
Yun-Chen Lo <tarzen0509@gmail.com>
2017-11-24 17:09:19 +0800

    part I finished

--- hw3/APSP_Pthread.c
+++ hw3/APSP_Pthread.c
@@ -7,20 +7,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <pthread.h>
 
 #define INF 999
 
-void floydWarshall (int *map, int vertice, int *dist, const char* outfile);
+typedef struct {
+    int *addr;
+    int l_start, l_end;
+    int vertice;
+    int k;
+    int id;
+}data;
+
+void floydWarshall (int *map, int vertice, int *dist, const char* outfile, int num_threads);
 
 void printSolution(int *map, int vertice, const char* outfile);
 
-void *Compare(void *dist);
+void *Compare(void *data_tmp);
 
 int main(int argc, char** argv){
     /* argument parsing */
 	assert(argc == 4);
     const char* infile = argv[1];
     const char* outfile = argv[2];
+    printf("%s\n", outfile);
 	int num_threads = strtol(argv[3], 0, 10);
 	printf("num_threads = %d \n", num_threads);
 	FILE * pFile;
@@ -60,42 +70,82 @@ int main(int argc, char** argv){
       }
 
     }
-
-    floydWarshall(map, vertice, dist, outfile);
+    floydWarshall(map, vertice, dist, outfile, num_threads);
 
   	fclose (pFile);
 
 	return 0;
 }
 
-void floydWarshall (int *map, int vertice, int *dist, const char* outfile){
-	
-	int i, j, k;
+void floydWarshall (int *map, int vertice, int *dist, const char* outfile, int num_threads){
+	printf("%s\n", outfile);
+	int tid, i, j, k;
 	for (i = 0; i < vertice; i++)
         for (j = 0; j < vertice; j++)
             dist[i*vertice + j] = map[i*vertice + j];
 
+    
+    data data_tmp[num_threads];
     for (k = 0; k < vertice; k++)
     {
-        for (i = 0; i < vertice; i++)
-        {
-            for (j = 0; j < vertice; j++)
-            {
-                if (dist[i*vertice + k] + dist[k*vertice + j] < dist[i*vertice + j])
-                    dist[i*vertice + j] = dist[i*vertice + k] + dist[k*vertice + j];
+        pthread_t threads[num_threads];
+        for(tid=0; tid<num_threads; tid++){
+            fprintf(stderr, "k, %d \n", k);
+            //fprintf(stderr, "hello, world\n", );
+            data_tmp[tid].addr = dist;
+            data_tmp[tid].l_start = (vertice*vertice/num_threads)*tid;
+            fprintf(stderr, "hello, %d \n", (vertice*vertice/num_threads)*tid);
+            fprintf(stderr, "vertice, %d \n", vertice);
+            data_tmp[tid].vertice = vertice;
+            data_tmp[tid].k = k;
+            data_tmp[tid].id = tid;
+            if(tid == num_threads-1){
+                data_tmp[tid].l_end = vertice*vertice;
+                fprintf(stderr, "l_end = %d \n", vertice*vertice);
             }
+            else{
+                data_tmp[tid].l_end = (vertice*vertice/num_threads)*(tid+1);
+                fprintf(stderr, "l_end = %d \n", (vertice*vertice/num_threads)*(tid+1));
+            }
+            
+            pthread_create(&threads[tid], NULL, Compare, (void *)&data_tmp[tid]);
         }
+        for(int tid=0; tid<num_threads; tid++){
+            pthread_join(threads[tid], NULL);
+        }   
+
     }
+
     printSolution(dist, vertice, outfile);
+    pthread_exit(NULL);
+    
 }
 
-void *Compare(void *dist){
-	int *dist_thread = (int *)dist;
+void *Compare(void *data_tmp){
+    
+	data *data_thread = (data *)data_tmp;
+    int *dist;
+    dist = data_thread->addr;
 
+    int l, i, j, k, vertice;
+    //fprintf(stderr, "dist[0]= %d \n", data_thread->addr[0]);
+    //fprintf(stderr, "start, end= %d %d\n", data_thread->l_start, data_thread->l_end);
+    k = data_thread->k;
+    vertice = data_thread->vertice;
+    for(l=data_thread->l_start; l<data_thread->l_end; l++){
+        //fprintf(stderr, "l= %d \n", l);
+        i=l/vertice;
+        j=l%vertice;
+        //fprintf(stderr, "i,j= %d %d \n", i, j);
+        if (dist[i*vertice + k] + dist[k*vertice + j] < dist[i*vertice + j])
+            dist[i*vertice + j] = dist[i*vertice + k] + dist[k*vertice + j];
+    }
+    pthread_exit(NULL); 
 }
 
 void printSolution(int *dist, int vertice, const char* outfile){
 	int i, j;
+    fprintf(stderr," %s\n", outfile);
 	FILE *out;
 	out=fopen(outfile, "w");
 	for (i = 0; i <  vertice; i++){
